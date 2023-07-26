841d3037313a87566843a4a319148c0d532f6471
Sumon Mitra <96655128+PassionateSumon@users.noreply.github.com>
2023-07-26 15:23:59 +0530

    Time: 5 ms (22.95%), Space: 6.6 MB (19.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -17,8 +17,23 @@ int rec(int row, int col, int rDes, int cDes, vector<vector<int>> &dp) {
     return dp[row][col] = (down + right);
 }
 public:
+    // <<-------- Memoization ------->>
+    // int uniquePaths(int m, int n) {
+    //     vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+    //     return rec(0, 0, m-1, n-1, dp);
+    // }
+
+    // <<----------- Tabulation ---------->>
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return rec(0, 0, m-1, n-1, dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 1));
+        dp[m-1][n-1] = 1;
+        for(int row = m-2; row >= 0; row--) {
+            for(int col = n-2; col >= 0; col--) {
+                int down = dp[row+1][col];
+                int right = dp[row][col+1];
+                dp[row][col] = down + right;
+            }
+        }
+        return dp[0][0];
     }
 };
