064388cab2ec61525320c67f0ce712f16cc75989
Sumon Mitra <96655128+PassionateSumon@users.noreply.github.com>
2023-07-26 16:49:29 +0530

    Time: 4 ms (56.51%), Space: 7.8 MB (24.81%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -19,9 +19,32 @@ int rec(int row, int col, vector<vector<int>> &nums, vector<vector<int>> &dp) {
     return dp[row][col] = (down + right);
 }
 public:
+    // int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+    //     int m = grid.size(), n = grid[0].size();
+    //     vector<vector<int>> dp(m, vector<int>(n, -1));
+    //     return rec(0, 0, grid, dp);
+    // }
+
+    // <<----------- Tabulation ---------->>
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return rec(0, 0, grid, dp);
+        vector<vector<long long>> dp(m, vector<long long>(n, 1));
+        for(int row = m-1; row >= 0; row--) {
+            for(int col = n-1; col >= 0; col--) {
+                if(grid[row][col] == 1) {
+                    dp[row][col] = 0;
+                } else if(row == m-1 && col == n-1) {
+                    dp[row][col] = 1;
+                } else {
+                    long long down = 0; 
+                    if(row < m-1) down = dp[row+1][col];
+                    long long right = 0; 
+                    if(col < n-1) right = dp[row][col+1];
+
+                    dp[row][col] = down + right;
+                }
+            }
+        }
+        return (int)dp[0][0];
     }
 };
