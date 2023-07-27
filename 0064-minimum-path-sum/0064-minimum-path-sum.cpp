776b20d4257591f8e8bfb527fc268304f0306278
Sumon Mitra <96655128+PassionateSumon@users.noreply.github.com>
2023-07-27 23:32:06 +0530

    Time: 11 ms (64.91%), Space: 10.1 MB (13.76%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,26 +1,27 @@
 class Solution {
-private:
-int rec(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-    // Pruning 
-    if(row < 0 || col < 0) return 100000;
+// private:
+// int rec(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+//     // Pruning 
+//     if(row < 0 || col < 0) return 100000;
 
-    // Base case 
-    if(row == 0 && col == 0) return grid[0][0];
+//     // Base case 
+//     if(row == 0 && col == 0) return grid[0][0];
 
-    // Cache check 
-    if(dp[row][col] != -1) return dp[row][col];
+//     // Cache check 
+//     if(dp[row][col] != -1) return dp[row][col];
 
-    // Compute
-    int up = grid[row][col] + rec(row - 1, col, grid, dp);
-    int left = grid[row][col] + rec(row, col -1, grid, dp);
+//     // Compute
+//     int up = grid[row][col] + rec(row - 1, col, grid, dp);
+//     int left = grid[row][col] + rec(row, col -1, grid, dp);
 
-    // Return --> Save and return
-    return dp[row][col] = min(up, left);
-}
+//     // Return --> Save and return
+//     return dp[row][col] = min(up, left);
+// }
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
         // Tabulation -->
         for(int i = 0; i < n; i++) {
             for(int j =0; j < m; j++) {
@@ -33,7 +34,6 @@ public:
                 }
             }
         }
-        // return rec(n-1, m-1, grid, dp);
         return dp[n-1][m-1];
     }
 };
