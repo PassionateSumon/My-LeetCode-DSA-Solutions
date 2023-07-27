4709b408617f90c0a96c299437e2d4b04b4ea4e7
Sumon Mitra <96655128+PassionateSumon@users.noreply.github.com>
2023-07-27 23:30:29 +0530

    Time: 7 ms (93.28%), Space: 10 MB (71.02%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -20,7 +20,22 @@ int rec(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp) {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return rec(n-1, m-1, grid, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        // Tabulation -->
+        for(int i = 0; i < n; i++) {
+            for(int j =0; j < m; j++) {
+                if(i == 0 && j == 0) dp[i][j] = grid[i][j];
+                else {
+                    int up = grid[i][j], left = grid[i][j];
+                    if(i > 0) up += dp[i-1][j];
+                    else up = 1e9;
+                    if(j > 0) left += dp[i][j - 1];  
+                    else left = 1e9;       
+                    dp[i][j] = min(up, left);
+                }
+            }
+        }
+        // return rec(n-1, m-1, grid, dp);
+        return dp[n-1][m-1];
     }
 };
