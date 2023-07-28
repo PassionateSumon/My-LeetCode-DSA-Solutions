cb497aed955a9085cc1db000e532c3f7a280e4e1
Sumon Mitra <96655128+PassionateSumon@users.noreply.github.com>
2023-07-28 13:52:25 +0530

    Time: 4 ms (87.72%), Space: 8.9 MB (46.95%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -19,7 +19,18 @@ int rec(int row, int col, vector<vector<int>> &tri, vector<vector<int>>& dp) {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return rec(0, 0, triangle, dp);
+        vector<vector<int>> dp(n, vector<int>(n+1, 0));
+        for(int j = 0; j < n; j++) {
+            dp[n-1][j] = triangle[n-1][j];
+        }
+
+        for(int i = n-2; i >= 0; i--) {
+            for(int j = i; j >= 0; j--) {
+                int curr = triangle[i][j] + dp[i+1][j];
+                int currPlusOne = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(curr, currPlusOne);
+            }
+        }
+        return dp[0][0];
     }
 };
