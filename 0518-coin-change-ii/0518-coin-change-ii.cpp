bc7ea3e34ef90d86f02f5a9ede5c86ad1fda09f1
Sumon Mitra <96655128+PassionateSumon@users.noreply.github.com>
2023-08-02 11:04:20 +0530

    Time: 39 ms (46.87%), Space: 18.5 MB (49.16%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -21,10 +21,32 @@ int rec(int ind, int tar, vector<int> &nums, vector<vector<int>> &dp) {
     return dp[ind][tar] = (take + notTake);
 }
 public:
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
-        int ans = rec(n-1, amount, coins, dp);
-        return ans;
+    // int change(int amount, vector<int>& coins) {
+    //     int n = coins.size();
+    //     vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
+    //     int ans = rec(n-1, amount, coins, dp);
+    //     return ans;
+    // }
+
+    int change(int target, vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
+        for(int i =0; i <= target; i++) {
+            if((i%nums[0]) == 0) dp[0][i] = 1;
+            else dp[0][i] = 0;
+        }
+
+        for(int ind = 1; ind < n; ind++) {
+            for(int tar = 0; tar <= target; tar++) {
+                int notTake = dp[ind-1][tar];
+                int take = 0;
+                if(nums[ind] <= tar) {
+                    take = dp[ind][tar-nums[ind]];
+                }
+
+                dp[ind][tar] = (take+notTake);
+            }
+        }
+        return dp[n-1][target];
     }
 };
