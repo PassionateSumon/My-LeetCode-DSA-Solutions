54c5b5dcc923ecd91ca788bd642e1e3f2e70903f
Sumon Mitra <96655128+PassionateSumon@users.noreply.github.com>
2023-08-19 19:56:47 +0530

    Time: 7 ms (82.48%), Space: 10.7 MB (24.95%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& nums) {
-        vector<pair<int, int>> mp;
         int n = nums.size();
-        for(int i = 0; i < n; ++i) {
-            mp.push_back({nums[i], __builtin_popcount(nums[i])});
-        }
-        // if(mp.size() == 1) {
-        //     sort(nums.begin(), nums.end());
-        //     return nums;
+        // vector<pair<int, int>> mp;
+        // for(int i = 0; i < n; ++i) {
+        //     mp.push_back({nums[i], __builtin_popcount(nums[i])});
         // }
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        for(auto it: mp) {
-            int num = it.first;
-            int bits = it.second;
+        for(int i = 0; i < n; ++i) {
+            int num = nums[i];
+            int bits = __builtin_popcount(num);
             pq.push({bits, num});
         } 
         vector<int> ans;
