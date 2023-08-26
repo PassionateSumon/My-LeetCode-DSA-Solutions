af658a05b20aef182d85b6cbefa54d9fd044ddab
Sumon Mitra <96655128+PassionateSumon@users.noreply.github.com>
2023-08-26 21:08:49 +0530

    Time: 35 ms (5.08%), Space: 24.6 MB (55.38%) - LeetHub

--- 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.cpp
+++ 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.cpp
@@ -2,36 +2,43 @@ class Solution {
 // I have to create an unordered map to store sum and their freq
 unordered_map<int, int> mp;
 private:
-    int rec(TreeNode* root) {
+    int rec(TreeNode* root, int &high) {
         int sum = 0, lsum = 0, rsum = 0;
 
-        if(root->left) lsum = rec(root->left);
-        if(root->right) rsum = rec(root->right);
+        if(root->left) lsum = rec(root->left, high);
+        if(root->right) rsum = rec(root->right, high);
 
         sum = lsum + rsum + root->val;
         mp[sum]++;
+        high = max(high, mp[sum]);
         return sum;
     }
 public:
     vector<int> findFrequentTreeSum(TreeNode* root) {
         vector<int> ans;
-        rec(root);
-        priority_queue<pair<int, int>> pq;
-        for(auto it: mp) {
-            pq.push({it.second, it.first});
-        }
+        int high = 0;
+        rec(root, high);
+        // cout << high ;
+        // priority_queue<pair<int, int>> pq;
+        // for(auto it: mp) {
+        //     pq.push({it.second, it.first});
+        // }
 
-        int mostFreq = pq.top().first;
-        int freqSum = pq.top().second;
-        pq.pop();
-        ans.push_back(freqSum);
-        while(!pq.empty()) {
-            auto ele = pq.top();
-            pq.pop();
-            int currFreq = ele.first;
-            int currEle = ele.second;
-            if(mostFreq > currFreq) break;
-            if(mostFreq == currFreq) ans.push_back(currEle);
+        // int mostFreq = pq.top().first;
+        // int freqSum = pq.top().second;
+        // pq.pop();
+        // ans.push_back(freqSum);
+        // while(!pq.empty()) {
+        //     auto ele = pq.top();
+        //     pq.pop();
+        //     int currFreq = ele.first;
+        //     int currEle = ele.second;
+        //     if(mostFreq > currFreq) break;
+        //     if(mostFreq == currFreq) ans.push_back(currEle);
+        // }
+
+        for(auto it: mp) {
+            if(it.second == high) ans.push_back(it.first);
         }
         return ans;
     }
